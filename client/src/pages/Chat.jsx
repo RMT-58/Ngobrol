7552dbd9a411938eeb15d6810c8b69e0f98b0611
dekalouis <dekarebaudo@gmail.com>
2025-03-04 17:33:23 +0700

    test: trying out socket features

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import ChatBox from "../components/ChatBox";
 import AllUsers from "../components/AllUser";
+import io from "socket.io-client";
+const socket = io("http://localhost:3000");
 
 const Chat = () => {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.emit("/messages/chat");
+
+    socket.on("/messages/chat", (data) => {
+      // console.log(data, "<<< dari server");
+      setMessages(data);
+    });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("/messages/chat/create", { message, sender: "Deka" });
+    setMessage("");
+  };
+
   return (
     <div>
-      <AllUsers />
-      <ChatBox />
+      {/* <AllUsers />
+      <ChatBox /> */}
+      <ul id="messages">
+        {messages.map((message, index) => (
+          <li key={index}>
+            {message.sender}:{message.message}
+          </li>
+        ))}
+      </ul>
+      <form id="form" onSubmit={handleSubmit}>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          id="input"
+          autocomplete="off"
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -3,18 +3,62 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
+
+//! SOCKET
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+//! SOCKET
+
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//http request
 app.get("/", (req, res) => {
-  res.send("Welcome to our chat API...");
+  res.send("Welcome to our chat API!");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}...`);
+//! SOCKET
+
+const chats = [];
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("/messages/chat", () => {
+    console.log("get chat");
+
+    socket.emit("/messages/chat", chats);
+  });
+
+  socket.on("/messages/chat/create", (data) => {
+    // console.log(data, `<<< dari klien`);
+    //harusnya dikirim ke database Message.create({...data})
+    chats.push(data);
+
+    io.emit("/messages/chat", chats);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
+
+// app.listen(port, () => {
+//   console.log(`Server running on port: ${port}...`);
+// });
+//! SOCKET
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
+//! SOCKET
 
 module.exports = app;
