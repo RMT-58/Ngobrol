34e4f56b2781d095c77f2292e0331d6c218d4416
thomy-fauzi <thomy.learning@gmail.com>
2025-03-05 00:39:59 +0700

    Feat: Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Chat from "./pages/Chat";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<PublicLayout />}> */}
-        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<h1>Register</h1>} />
         {/* </Route> */}
 
